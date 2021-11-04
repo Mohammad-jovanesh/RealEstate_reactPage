46ed7ca8bf74a4219a924e4b73ed6b964e9f01ef
unknown <-v>
2021-11-04 00:49:54 -0700

    make some useEffact useState for animation Scroll Event

--- src/Component/Header/Haeder.jsx
+++ src/Component/Header/Haeder.jsx
@@ -1,22 +1,36 @@
-import React from 'react';
-import {TopHeader,MyLogo,NavBar,HeaderLink} from "./Header.js"
+import React,{useEffect,useState}from 'react';
+import {TopHeader,MyLogo,NavBar,HeaderLink,TopHeader_Scroll} from "./Header.js"
 import {navkinks_Data} from "./navLink_data.js"
 import {NavLink} from "react-router-dom"
 import {BarBtn} from "../Bar_Btn/BatBtn.jsx"
 
 export const Header=()=>{
+  const [headerChange,setHeaderChange]=useState(false)
+  useEffect(()=>{
+     window.addEventListener("scroll",(e)=>{
+         console.log()
+         if(window.scrollY>100){
+          setHeaderChange(true)
+         }
+         if(window.scrollY<100){
+          setHeaderChange(false)
+         }
+     })
+  },[])
     return (
-      <TopHeader>
-            <MyLogo to="/">
+      <>     
+      <TopHeader ScrollEffect={headerChange}>
+            <MyLogo to="/" ScrollEffect={headerChange}>
                 MP
             </MyLogo>
             <NavBar>
-              {navkinks_Data.map((Elm,Index)=>{return(<HeaderLink to={Elm.path}  key={Index}>{Elm.title}
+              {navkinks_Data.map((Elm,Index)=>{return(<HeaderLink  ScrollEffect={headerChange} to={Elm.path}  key={Index}>{Elm.title}
               
               
                </HeaderLink>)})}
                
             </NavBar>  
       </TopHeader>
+      </>
     )
 }
