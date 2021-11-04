7ce5db92e56c0bd185166b495e7405b2e08d721e
unknown <-v>
2021-11-04 00:53:39 -0700

    make Card component for Service section and styled them like Card

--- src/Component/Service_Section/ServiceSection.jsx
+++ src/Component/Service_Section/ServiceSection.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import {ServicesSection} from "./ServiceSection_Style.js"
-
+import React,{useEffect,useState} from 'react'
+import {ServicesSection
+,ServicesSection_title
+,ServicesSection_Grid
+} from "./ServiceSection_Style.js"
+import {ServicesCard} from "./ServicesCard/ServicesCard.jsx"
+import {ServicesCardData} from "./ServicesCard/Services_Card_Data.js"
 export const ServiceSection=()=>{
+    const [Cardlist,SetCardList]=useState(null)
+    useEffect(() => {
+        SetCardList(ServicesCardData)
+    }, []);
     return(
         <ServicesSection>
-           <ServicesSection_Title></ServicesSection_Title>  
+           <ServicesSection_title>Some Services Of Us</ServicesSection_title>
+           <ServicesSection_Grid>
+              {Cardlist&&Cardlist.map((card,index)=>{
+                  return <ServicesCard  title={card.title} icon={card.icon}/>
+              })}
+               
+               </ServicesSection_Grid>
         </ServicesSection>
     )
 }
--- src/Component/Service_Section/ServiceSection_Style.js
+++ src/Component/Service_Section/ServiceSection_Style.js
@@ -5,6 +5,7 @@ export const ServicesSection =styled.section`
 width: 100%;
 height: 100vh;
 position: relative;
+padding: 5rem 4rem 2rem;
 &::after{
     position: absolute;
     content: "";
@@ -14,6 +15,23 @@ position: relative;
     height: 3px;
     background: #f1f1f1;
     border-radius: 1rem;
+   
     
 }
+
+`
+export const ServicesSection_title=styled.h1`
+color: #444;
+text-transform: uppercase;
+text-shadow:1px 1px 5px rgba(0,0,0,0.2);
+font-size:2rem;
+`
+
+export const ServicesSection_Grid=styled.div`
+ display: grid;
+ grid-gap: 2rem;
+ grid-template-columns: repeat(auto-fit,minmax(300px,1fr));
+ grid-template-rows: repeat(1,1fr);
+ margin-top:8rem ;
+
 `
